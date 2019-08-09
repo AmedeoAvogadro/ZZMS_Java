3adff7f9f9445bf8705caa605d6d87628a548212
焦一知 Phillip <1109644364@qq.com>
2019-08-09 15:10:49 +0800

    ...

--- src/LongInteger/LongInteger.java
+++ src/LongInteger/LongInteger.java
@@ -1,6 +1,7 @@
 package LongInteger;
 
 import java.util.*;
+
 import java.io.*;
 
 public class LongInteger {
--- src/LongInteger/Process.java
+++ src/LongInteger/Process.java
@@ -1,12 +1,13 @@
 package LongInteger;
 
 import java.util.*;
+
 import java.io.*;
 
 public class Process {
     public static void main(String[] args) throws IOException {
-        File in_f = new File("C:\\Users\\11096\\IdeaProjects\\ZZMS\\src\\LongInteger\\Long_Input.txt");
-        File out_f = new File("C:\\Users\\11096\\IdeaProjects\\ZZMS\\src\\LongInteger\\Long_Output.txt");
+        File in_f = new File("C:\\Users\\11096\\IdeaProjects\\ZZMS_Java\\src\\LongInteger\\Long_Input.txt");
+        File out_f = new File("C:\\Users\\11096\\IdeaProjects\\ZZMS_Java\\src\\LongInteger\\Long_Output.txt");
         InputStream in_s = new FileInputStream(in_f);
         OutputStream out_s = new FileOutputStream(out_f);
         Scanner scnr = new Scanner(in_s);
